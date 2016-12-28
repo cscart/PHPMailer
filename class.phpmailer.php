1a4a3596afad5235fd1600e910882493e1efd087
Mike Schekotov <mschekotov@cs-cart.com>
2016-12-28 16:33:42 +0400

    [!] {@17995} CVE-2016-10045 worked around.

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -1028,7 +1028,7 @@ class PHPMailer
         $this->FromName = $name;
         if ($auto) {
             if (empty($this->Sender)) {
-                $this->Sender = $address;
+                $this->Sender = escapeshellcmd($address) === $address ? $address : ''; 
             }
         }
         return true;
